d146e1a9278f32f66ef3a7c7296bce57ad9da68c
Matthew-Godstime <99479350+Matthew-Godstime@users.noreply.github.com>
2022-06-09 17:47:02 +0100

    Update app.component.ts
    Changed the countdown call method

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TimerService } from './timer.service';
 
 @Component({
@@ -6,8 +6,12 @@ import { TimerService } from './timer.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "Stime's TODO List App";
   constructor(public ts: TimerService) { ts.dayCount(); ts.weekCount(); }
+  
+    ngOnInit(): void {
+    this.ts.dayCount(); this.ts.weekCount();
+  }
   
 }
